8063d7d8ec34b8c441e1b928a6e5869d24e70809
Pl4yj4ck <106487475+Pl4yj4ck@users.noreply.github.com>
2026-02-11 19:57:01 +0100

    add file

---
+++ db/dati.js
@@ -0,0 +1,11 @@
+// data.js
+// Inizializzazione vuota (verrà popolata dalle impostazioni)
+
+localStorage.removeItem("saldi");
+
+// esempio struttura finale salvata:
+// {
+//   "MACCHINA": 1001,
+//   "CANE": 1001,
+//   "GATTO": 1001
+// }
